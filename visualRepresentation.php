0f8f6adf3f0eb6b391d6c86a01f467414a97b178
munkbusiness <krudtso@gmail.com>
2014-10-31 13:23:56 +0100

    Seperator added. Style problems with overflow and line-break fixed. More boxes for theoretical testing added.

--- visualRepresentation.php
+++ visualRepresentation.php
@@ -7,12 +7,18 @@
 	<body>
 		<div id="wrapper">
 			<div id="frame">
+				<img id="seperator" src="images/seperator.png" />
+				
 				<div id="outerBox">
 					<div id="box1">
 						
 					</div><div id="box2">
 						
 					</div><div id="box3">
+						
+					</div><div id="box4">
+						
+					</div><div id="box5">
 						
 					</div>
 				</div>
