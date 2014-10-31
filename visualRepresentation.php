a20aa2eae6c7485fa2749b769fe0ecc988ec4cbc
munkbusiness <krudtso@gmail.com>
2014-10-31 09:41:11 +0100

    VisualRepresentation + style sheet added

---
+++ visualRepresentation.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Visual Representation</title>
+		<link href="style/mainStyle.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="frame">
+				<div id="outerBox">
+					<div id="box1">
+						
+					</div>
+					<div id="box2">
+						
+					</div>
+					<div id="box3">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		
+	</body>
+</html>
