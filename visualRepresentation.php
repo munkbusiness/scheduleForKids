13f20b4ff75e2189ce5607de187d05a4778b631e
peterhallum <peter91@cs.au.dk>
2014-11-17 17:26:35 +0100

    Brede fix

--- visualRepresentation.php
+++ visualRepresentation.php
@@ -10,17 +10,26 @@
 				<img id="seperator" src="images/seperator.png" />
 				
 				<div id="outerBox">
-					<div id="box1">
-						kl. 8.00
-					</div><div id="box2">
-						Kl. 8.30
-					</div><div id="box3">
-						kl. 9.00
-					</div><div id="box4">
-						kl. 10.00
-					</div><div id="box5">
-						kl. 14.00
+					<div id="box45" style="background-color: #eef2cc; background-image: url(images/matematik.png); background-size: 390px">
+						<p class="clock"> &nbsp; 8:00</p>
+					</div><div id="box45" style="background-color: #e1f0e3; background-image: url(images/geografi.png); background-size: 390px">
+						&nbsp; 8:45
+					</div><div id="box20" style="background-color: #b9d7ee; background-image: url(images/freetyme.png); background-size: 190px">
+						&nbsp; 9:30
+					</div><div id="box60" style="background-color: #fdece6; ">
+						&nbsp; 9:50
+					</div><div id="box45" style="background-color: #f8f3e6">
+						&nbsp; 10:50
+					</div><div id="box45pause">
+						&nbsp; 11:35
+					</div><div id="box45">
+						&nbsp; 12:20
+					</div><div id="box45">
+						&nbsp; 13:05
+					</div><div id="box45">
+						&nbsp; 14:00
 					</div>
+                    
 				</div>
 			</div>
 		</div>
